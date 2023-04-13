0094626c95c30de557dec64a488f6bbd0a358574
Satya Yoganand <satyaadd22@gmail.com>
2023-04-13 10:13:11 -0500

    Add files via upload

---
+++ SE452/src/main/java/com/example/SE452/Se452Application.java
@@ -0,0 +1,13 @@
+package com.example.SE452;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Se452Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Se452Application.class, args);
+	}
+
+}
---
+++ SE452/src/test/java/com/example/SE452/Se452ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SE452;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Se452ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
